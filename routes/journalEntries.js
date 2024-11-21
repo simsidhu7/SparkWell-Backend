07c981763bf5fa3dabff2cc6af71053c136542f5
Simrit Sidhu <simmysidhu@mac.home>
2024-11-20 23:26:57 -0500

    Create journal entries page get and post requests

--- routes/journalEntries.js
+++ routes/journalEntries.js
@@ -3,7 +3,45 @@ import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
-const newEntry ={
-    id:uuidv4(), 
-    entry
-}
+const journalEntriesFile = "./data/journalentries.json";
+
+router.get("/", (req, res) => {
+  fs.readFile(journalEntriesFile, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Cannot read journal entries" });
+    }
+    const journalEntries = JSON.parse(data || "[]");
+    res.json(journalEntries);
+  });
+});
+
+router.post("/", (req, res) => {
+  const { entry } = req.body;
+
+  const newEntry = {
+    id: uuidv4(),
+    entry,
+    timestamp: new Date().toISOString().split("T")[0],
+  };
+
+  fs.readFile(journalEntriesFile, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Cannot read journal entry." });
+    }
+    const journalEntries = JSON.parse(data || "[]");
+    journalEntries.push(newEntry);
+
+    fs.writeFile(
+      journalEntriesFile,
+      JSON.stringify(journalEntries, null, 2),
+      (err) => {
+        if (err) {
+          return res.status(500).json({ error: "Cannot save journal entry." });
+        }
+        res.status(201).json(newEntry);
+      }
+    );
+  });
+});
+
+export default router;
